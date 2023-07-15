acbaf2f9abb1dcd7955c83954279d52290466eec
Owen <kimdosa1212@naver.com>
2023-07-15 20:06:05 +0900

    Initial commit

---
+++ src/test/java/Ksh/firstspring/FirstSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Ksh.firstspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
